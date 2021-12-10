fd8d7d4fb71845e60e0b7c18e4038e9369cf9ab7
Tony Desktop <tcheng69@my.bcit.ca>
2021-12-10 11:56:26 -0800

    added event listener to switch theme with button

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,15 @@
 const toggle = document.querySelector(".toggle")
+const body = document.querySelector("body")
+
+toggle.addEventListener("click",(e)=>{
+    if (body.classList.contains("light-theme")) {
+        body.classList.remove("light-theme")
+        body.classList.add("dark-theme")
+    }
+    else {
+        body.classList.remove("dark-theme")
+        body.classList.add("light-theme")
+    }
+
+
+})
